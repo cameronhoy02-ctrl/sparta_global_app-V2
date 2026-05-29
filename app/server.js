eccf99a4110bb9da7d9dc1af2917e932c76012d9
Cameron <cameronhoy02@gmail.com>
2026-05-29 14:02:10 +0100

    new test time 4

--- app/server.js
+++ app/server.js
@@ -97,7 +97,7 @@ function getMongoFallbackMessage(reason) {
 }
 
 function getFooterVersionStamp() {
-	const configuredTimestamp = String(process.env.APP_FOOTER_TIMESTAMP || '29/05/2026 13:13pm').trim();
+	const configuredTimestamp = String(process.env.APP_FOOTER_TIMESTAMP || '29/05/2026 14.01pm').trim();
 	return configuredTimestamp ? `${APP_VERSION} ${configuredTimestamp}` : APP_VERSION;
 }
 
